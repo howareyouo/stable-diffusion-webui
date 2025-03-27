f406bee3ecb72b046d1c2528fca4eef66710c7b1
backflow <hunan_me@163.com>
2025-03-27 19:22:36 +0800

    my change

--- webui-user.bat
+++ webui-user.bat
@@ -5,20 +5,32 @@ set GIT=
 set VENV_DIR=
 set SAFETENSORS_FAST_GPU=1
 
-REM pip install torch==2.1.0+cu118 torchvision --force-reinstall --index-url https://download.pytorch.org/whl/cu118
-REM https://github.com/AUTOMATIC1111/stable-diffusion-webui/wiki/Optimizations
+rem pip install torch==2.1.0+cu118 torchvision --force-reinstall --index-url https://download.pytorch.org/whl/cu118
+rem https://github.com/AUTOMATIC1111/stable-diffusion-webui/wiki/Optimizations
 
 rem --no-half --upcast-sampling
+
+rem set COMMANDLINE_ARGS=^
+rem 	--skip-version-check ^
+rem 	--skip-prepare-environment ^
+rem 	--disable-safe-unpickle ^
+rem 	--disable-nan-check ^
+rem 	--no-half-vae ^
+rem 	--opt-split-attention ^
+rem     --opt-sdp-attention ^
+rem 	--opt-sdp-no-mem-attention ^
+rem 	--opt-sub-quad-attention ^
+rem 	--opt-channelslast ^
+rem 	--upcast-sampling ^
+rem 	--xformers
 
 set COMMANDLINE_ARGS=^
 	--skip-version-check ^
 	--skip-prepare-environment ^
 	--disable-nan-check ^
 	--disable-safe-unpickle ^
-	--no-half-vae ^
 	--opt-split-attention ^
-	--opt-sub-quad-attention ^
-	--upcast-sampling ^
+	--opt-sdp-attention ^
 	--opt-channelslast ^
 	--xformers
 
